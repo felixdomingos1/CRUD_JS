d3074a898e5623d741d601a2372687f120ed22ee
felixdomingos1 <felixsdomingos93@gmail.com>
2023-09-15 00:00:16 +0100

    add my getItensBD func t0 get a items in my DB

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function loadItens() {
 
 }
 
-// const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 // const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
 // loadItens()
