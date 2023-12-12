668bacee19ad0a083be24d5fa6875e08954fda06
felixdomingos1 <felixsdomingos93@gmail.com>
2023-12-12 20:26:50 +0100

    New Updates

--- script.js
+++ script.js
@@ -90,15 +90,3 @@ const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
 loadItens()
-
-
-
-
-
-
-
-
-
-
-
- 
